64796083e40770ff93d1f6e5750785b7e86ce78e
Mustafa Ersin Şişman <musisman@k1m28s07.42istanbul.com.tr>
2024-11-28 19:33:12 +0300

    iskelet

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: musisman <musisman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 16:54:34 by musisman          #+#    #+#             */
+/*   Updated: 2024/11/26 17:04:04 by musisman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+char	*get_next_line(int fd);
+int		ft_putchar(int c);
+int		ft_putstr(char *s);
+
+#endif
