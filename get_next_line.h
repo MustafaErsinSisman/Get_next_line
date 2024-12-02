64f958172cf97a41fce6c04ac76d2961e2e51b25
Mustafa Ersin Şişman <mustafaersinsisman@gmail.com>
2024-12-02 14:42:18 +0300

    dandik fonksiyonlar yazıldı

--- get_next_line.h
+++ get_next_line.h
@@ -3,15 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: musisman <musisman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: musisman <<musisman@student.42.fr>>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 16:54:34 by musisman          #+#    #+#             */
-/*   Updated: 2024/11/26 17:04:04 by musisman         ###   ########.fr       */
+/*   Updated: 2024/12/02 14:14:43 by musisman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
 
 char	*get_next_line(int fd);
 int		ft_putchar(int c);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,30 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: musisman <musisman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: musisman <<musisman@student.42.fr>>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 16:55:39 by musisman          #+#    #+#             */
-/*   Updated: 2024/11/26 17:01:30 by musisman         ###   ########.fr       */
+/*   Updated: 2024/12/02 14:41:52 by musisman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_putchar(int c)
-{
-	write(1, &c, 1);
-	return (1);
-}
+#include "get_next_line_h"
 
-int	ft_putstr(char *s)
+size_t	ft_strlen(const char *s)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s, char *buffer)
+{
+	char	*a;
+	size_t	i;
+	size_t	j;
+
 	if (!s)
 	{
-		write(1, "(null)", 6);
-		return (6);
+		s = malloc(1);
+		s[0]  = '\0';
 	}
-	while (s[i])
-		ft_putchar(s[i++]);
-	return (i);
+	a = malloc(ft_strlen(s) + ft_strlen(buffer) + 1);
+	i = 0;
+	c = 0;
+	if (a == NULL)
+		return (NULL);
+	while (s[c])
+	{
+		a[c] = s[c];
+		c++;
+	}
+	while (buffer[i])
+		a[c++] = buffer[i++];
+	res[ft_strlen(s) + ft_strlen(buffer)] = '\0';
+	free(s);
+	return (a);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(s);
+	while (i <= len)
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	return (0);
 }
