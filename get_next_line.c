8fb275bbb169e415e3fe742c7d83bcda34dc77c9
Mustafa Ersin Şişman <79980235+MustafaErsinSisman@users.noreply.github.com>
2024-12-05 01:18:23 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -10,6 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char *ft_bufferstr(int fd, char *s)
+{
+  char *buffer;
+  int b;
+  
+  buffer = malloc(BUFFER_SIZE + 1);
+  if (!buffer)
+    return (NULL);
+  b = 1;
+  while (ft_strchr(s, '\n') == 0 && b != 0)
+  {
+    b = read(fd, buffer, BUFFER_SIZE);
+    if (i == -1)
+    {
+      free(buffer);
+      free(s);
+      return (NULL);
+    }
+  buffer[i] = '\0';
+  s = ft_strjoin(s, buffer);
+  }
+  free(buffer);
+  return (s);
+}
+
 char	*get_next_line(int fd)
 {
+  static char * s ;
+  char * ptr;
+
+  if ( fd < 0 && BUFFER_SIZE < 1 )
+      return (NULL);
+  s = ft_bufferstr(fd, s); // anlaşıldı ama detaylı bak yine
+  if( !s)
+  return (NULL);
+  ptr = line_s(s);
+  s = ft_new(s);
+  return (ptr);
 }
